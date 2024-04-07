a8d290a4cc9a24200b8c0bf858920bca1b45c209
jiayezi <1601235906@qq.com>
2024-04-07 20:55:23 +0800

    完成赋分配置的模型设计

--- exam/convert/models.py
+++ exam/convert/models.py
@@ -1,9 +1,11 @@
 import os
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class UploadedFile(models.Model):
     file = models.FileField(upload_to='uploaded_files/')
+    upload_date = models.DateTimeField('Upload date', auto_now_add=True)
 
     def delete(self, *args, **kwargs):
         if self.file:
@@ -11,3 +13,16 @@ class UploadedFile(models.Model):
             if os.path.exists(self.file.path):
                 os.remove(self.file.path)
         super(UploadedFile, self).delete(*args, **kwargs)
+
+
+class ConvertConfig(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    config_name = models.CharField(max_length=20)
+
+
+class Grade(models.Model):
+    config_name = models.ForeignKey(ConvertConfig, on_delete=models.CASCADE)
+    grade_name = models.CharField(max_length=4)
+    high_score = models.IntegerField()
+    low_score = models.IntegerField()
+    percent = models.IntegerField()
