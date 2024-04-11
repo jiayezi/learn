6cd71000706e472b744f1463ac3e16878f187cee
jiayezi <1601235906@qq.com>
2024-04-11 11:29:39 +0800

    添加css样式

---
+++ exam/static/customize/js/file_upload.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var fileInput = document.getElementById("id_file");
+    var uploadButton = document.querySelector(".button[type='submit']");
+    var fileNameDisplay = document.getElementById("file_name_display");
+
+    fileInput.addEventListener("change", function() {
+        if (fileInput.files.length > 0) {
+            fileNameDisplay.textContent = fileInput.files[0].name;
+        } else {
+            fileNameDisplay.textContent = "没有选择文件";
+        }
+    });
+
+    uploadButton.addEventListener("click", function(event) {
+        if (fileInput.files.length === 0) {
+            event.preventDefault(); // 阻止默认提交行为
+            fileNameDisplay.textContent = "没有选择文件";
+        }
+    });
+});
