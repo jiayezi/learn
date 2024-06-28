f9bd3c9c19b023dde335b9b7ac1ee132bc916e55
jiayezi <1601235906@qq.com>
2024-06-28 22:50:40 +0800

    使用requests实现，暂未测试

--- 爬虫/爬取1x图片.py
+++ 爬虫/爬取1x图片.py
@@ -1,56 +1,59 @@
-import urllib.request
-import os
+import requests
 from lxml import etree
-"""
+import re
+import json
+import time
+
+user_id = '148395'
+user_name = 'arcadiaman'
+
 # 第一步，获取每张图片的详情页面的网址
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'accept-language': 'zh-CN,zh;q=0.9',
+    'cache-control': 'max-age=0',
+    'cookie': '1xSession3=89a5a097bcc8fbe9a032aea2dd248ef7',
+    'referer': f'https://1x.com/{user_name}'}
+
+url = f'https://1x.com/backend/lm2.php?'
+params = {'style': 'normal', 'mode': f'user:{user_id}:overview:'}
 url_list = []
+
 for i in range(0, 201, 20):
-    url = f'https://1x.com/backend/lm2.php?style=normal&mode=user:148395:overview:&from={i}'
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
-        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-        'accept-language': 'zh-CN,zh;q=0.9',
-        'cache-control': 'max-age=0',
-        'cookie': '1xSession3=89a5a097bcc8fbe9a032aea2dd248ef7',
-        'referer': 'https://1x.com/arcadiaman'}
-    request = urllib.request.Request(url=url, headers=headers)
-    response = urllib.request.urlopen(request)
-    content = response.read().decode('utf8')
-    tmp_list = re.findall(r'/photo/\d+/useroverview/148395', content)
+    params['from'] = i
+    response = requests.get(url, params=params, headers=headers)
+    response.encoding = 'utf-8'
+    content = response.text
+    tmp_list = re.findall(rf'/photo/\d+/useroverview/{user_id}', content)
     url_list.extend(tmp_list)
+    time.sleep(1)
 
 print(len(url_list))
 
-with open('img_detail_url.txt', 'wt') as f:
-    for s in url_list:
-        f.write('https://1x.com'+s+'\n')
+url_dict = {'url': 'https://1x.com' + s for s in url_list}
 
-"""
+with open('img_detail_page_url.json', 'w') as f:
+    json.dump(url_dict, f)
+
 
 # 第二步，访问每个图片详情页并提取信息
-name_list = os.listdir(r'E:\行星鉴定记录者\图片\Michael Bilotta\1x.com arcadiaman')
-title_file = ''
-with open('img_detail_url.txt', 'rt') as f:
-    data = f.readlines()
+img_title_url_dict = {}
+with open('img_detail_page_url.json', encoding='utf-8') as f:
+    url_dict = json.load(f)
+    urls = url_dict['url']
     counter = 0
-    for url in data:
-        url = url.strip()
-        headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
-            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-            'accept-language': 'zh-CN,zh;q=0.9',
-            'cache-control': 'max-age=0',
-            'cookie': '1xSession3=89a5a097bcc8fbe9a032aea2dd248ef7',
-            'referer': 'https://1x.com/arcadiaman'}
-        request = urllib.request.Request(url=url, headers=headers)
-        response = urllib.request.urlopen(request)
+    for url in urls:
+        response = requests.get(url, headers=headers)
+        response.encoding = 'utf-8'
         counter += 1
-        content = response.read().decode('utf8')
+        content = response.text
         tree = etree.HTML(content)
-        title = tree.xpath('/html/head/title/text()')[0].replace(' by Michael Bilotta', '')
-        title_file += f'{title}\t{url}\n'
-        print(f'{counter:>3d}\t{title}\t{url}')
+        title = tree.xpath('//*[@id="photodata-2843318"]/div[1]/div[1]/text()')[0]
+        img_url = tree.xpath('//*[@id="img-2843320"]/@src')[0]
+        if title is None:
+            title = f'Untitled {counter:>3d}'
+        img_title_url_dict[title] = img_url
 
-with open('img_title_url.txt', 'wt', encoding='utf-8') as f:
-    f.write(title_file)
-
+with open('img_title_url_dict.json', 'w') as f:
+    json.dump(img_title_url_dict, f)
